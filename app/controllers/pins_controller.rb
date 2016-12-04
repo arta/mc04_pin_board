4b09ad00eb3d9bf232f6754ee683abb6b8f11eaf
Martin Fencl <martin.fencl@gmail.com>
2016-12-04 16:32:31 -0600

    Add devise authentication to pins controller and /show

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /pins
   # link_to .. pins_path
