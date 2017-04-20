41f6ae3bda9210d545e89aec2ba156217442278b
Martin Fencl <Arta@Dart.westell.com>
2017-04-20 13:09:06 -0500

    Tidy up creating pins comments and explanatory notes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,21 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   # POST /pins/:pin_id/comments
-  # pin_comments_path( @pin ), method: :post
-  # form_for [ @pin, @pin.comments.new ]       ... on pins#show page
+  #   html: <form action='/pins/1/comments' method='post' ..
+  #   haml: =form_for [ @pin, Comment.new ] ..       ... on /pins/show
+  #   router: resources :pins do .. resources :comments, only: :create ..
+  #           ( post '/pins/:pin_id/commets', to: comments#create, as: pin_comments )
+  #           ( pin_comments_path( @pin ), method: :post )
   def create
     @pin = Pin.find params[:pin_id]
     @comment = @pin.comments.new( comment_params )
     @comment.user = current_user
 
-    # no fussing with correcting errors (blank comment.body),
-    #   just type in the damn comment! :) (contrast with a01_raddit)
-    flash[:notice] = 'Thanks for commenting!' if @comment.save # don't save if :body not present
+    # No fussing with correcting comment's errors (i.e. @comment.body.blank?)
+    # = save if user types the @comment.body; @comment.valid? == true
+    # = don't save if user posts @comment.body.blank?; @comment.invalid? == true
+    # (contrast with a01_raddit)
+    flash[:notice] = 'Thanks for commenting!' if @comment.save
     redirect_to @pin
   end
 
